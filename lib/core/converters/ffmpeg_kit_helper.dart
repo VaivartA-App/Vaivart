9f18363d6804dde4024191cbe7458a97dd8d3824
Khutwad-Bhavesh <b.vs.khutwad@gmail.com>
2026-08-15 21:23:58 +0530

    chore: github contribution update 23

--- lib/core/converters/ffmpeg_kit_helper.dart
+++ lib/core/converters/ffmpeg_kit_helper.dart
@@ -1,4 +1,6 @@
 /// Conditional export helper for FFmpeg Kit on Flutter Android vs Desktop platforms.
+library;
+
 export 'ffmpeg_kit_stub.dart'
     if (dart.library.ui) 'ffmpeg_kit_android.dart';
 
